31e24746f9d96ddb593e28e1f19f2d74dbcdc556
mnevrov <nev6@mail.ru>
2026-08-15 09:27:48 +0300

    test trailer frame header encoding

--- tests/unit/test_frame.c
+++ tests/unit/test_frame.c
@@ -4,8 +4,8 @@
 
 #include "../../src/grpc_web_frame.h"
 
-int
-main(void)
+static void
+test_parser(void)
 {
     grpc_web_frame_parser_t p;
     uint8_t frame[] = {0x00, 0x00, 0x00, 0x00, 0x03, 'a', 'b', 'c'};
@@ -29,7 +29,27 @@ main(void)
     n = grpc_web_frame_consume(&p, frame + 6, 2);
     assert(n == 2);
     assert(grpc_web_frame_is_complete(&p));
+}
 
+static void
+test_trailer_header_writer(void)
+{
+    uint8_t header[GRPC_WEB_FRAME_HEADER_SIZE];
+
+    grpc_web_frame_write_header(header, GRPC_WEB_TRAILER_FLAG, 0x01020304u);
+
+    assert(header[0] == 0x80);
+    assert(header[1] == 0x01);
+    assert(header[2] == 0x02);
+    assert(header[3] == 0x03);
+    assert(header[4] == 0x04);
+}
+
+int
+main(void)
+{
+    test_parser();
+    test_trailer_header_writer();
     puts("frame tests: ok");
     return 0;
 }
